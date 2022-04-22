0a93275f7a59ecee77f047072d7fb4f7149802f2
Mohamed Kassou <kassoumohammed@outlook.com>
2022-04-22 02:23:39 +0000

    ani files

---
+++ ANI/src/main/java/com/kscodes/ani/ANChooser.java
@@ -0,0 +1,19 @@
+package com.kscodes.ani;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ANChooser {
+
+    public static void ANIronSourceBanner(Context c){
+        Toast.makeText(c, "ANIronSource", Toast.LENGTH_SHORT).show();
+    }
+
+    public static void ANApplovinMaxBanner(Context c){
+        Toast.makeText(c, "ANApplovinMax", Toast.LENGTH_SHORT).show();
+    }
+
+    public static void ANAdmobBanner(Context c){
+        Toast.makeText(c, "ANAdmob", Toast.LENGTH_SHORT).show();
+    }
+}
